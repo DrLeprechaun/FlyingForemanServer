14c079f08e5e4fb81b369abed56a24274381357d
Vadim <leprechaunvanrosenkreuz@gmail.com>
2017-11-10 19:47:04 +0300

    Initial Commit

---
+++ src/main/java/com/gakeko/ffserver/FlyingForemanServerApplication.java
@@ -0,0 +1,12 @@
+package com.gakeko.ffserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingForemanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingForemanServerApplication.class, args);
+	}
+}
