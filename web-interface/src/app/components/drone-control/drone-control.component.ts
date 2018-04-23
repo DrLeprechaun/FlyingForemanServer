86a474d264308722b6b274e8e08306840f6b8409
Vadim <leprechaunvanrosenkreuz@gmail.com>
2018-04-23 23:13:10 +0300

    Control interface

--- web-interface/src/app/components/drone-control/drone-control.component.ts
+++ web-interface/src/app/components/drone-control/drone-control.component.ts
@@ -14,12 +14,19 @@ export class DroneControlComponent implements OnInit {
 data: any = null;
 logColor: String = "green";
 joystick1Pressed: boolean = false;
+joystick2Pressed: boolean = false;
 joystick1X:number = 0;
 joystick1Y:number = 0;
+joystick2X:number = 0;
+joystick2Y:number = 0;
 cx1:number = 50;
 cy1:number = 50;
 j1x:number = 0;
 j1y:number = 0;
+cx2:number = 50;
+cy2:number = 50;
+j2x:number = 0;
+j2y:number = 0;
 
 
 constructor(
@@ -31,16 +38,18 @@ ngOnInit(): void {
   /*this.joyStick();*/
   document.getElementById("joyStick1Button").addEventListener('mousedown', this.joystick1OnTouchStart.bind(this));
   document.getElementById("joyStick1Button").addEventListener('mouseup', this.joystick1OnTouchStop.bind(this));
+  document.getElementById("joyStick2Button").addEventListener('mousedown', this.joystick2OnTouchStart.bind(this));
+  document.getElementById("joyStick2Button").addEventListener('mouseup', this.joystick2OnTouchStop.bind(this));
   document.body.addEventListener('mouseup', this.bodyMouseDown.bind(this));
   document.addEventListener('keydown', this.onKeydown.bind(this));
   document.addEventListener('keyup', this.onKeyup.bind(this));
   document.getElementById("joyStick1Button").addEventListener('mousemove', this.joystick1Drag.bind(this));
+  document.getElementById("joyStick2Button").addEventListener('mousemove', this.joystick2Drag.bind(this));
   this.joystick1X = document.getElementById("joystick1").clientWidth / 2;
   this.joystick1Y = document.getElementById("joystick1").clientHeight / 2;
 }
 
 private executeCommand(cmd) {
-  //return this._http.get('http://178.63.57.162:8080/executeCommand?cmd=' + cmd, {headers: this.getHeaders()})
   return this._http.get('http://localhost:8080/executeCommand?cmd=' + cmd, {headers: this.getHeaders()})
               .map((res: Response) => res.json())
                .subscribe(data => {
@@ -65,6 +74,11 @@ private bodyMouseDown(): void {
     this.cx1 = 50;
     this.cy1 = 50;
   }
+  if (this.joystick2Pressed) {
+    this.joystick2Pressed = false;
+    this.cx2 = 50;
+    this.cy2 = 50;
+  }
 }
 
 private joystick1OnTouchStart(event): void {
@@ -73,10 +87,22 @@ private joystick1OnTouchStart(event): void {
   this.j1y = event.offsetY?(event.offsetY):event.pageY-document.getElementById("joystick1").offsetTop;
 }
 
+private joystick2OnTouchStart(event): void {
+  this.joystick2Pressed = true;
+  this.j2x = event.offsetX?(event.offsetX):event.pageX-document.getElementById("joystick2").offsetLeft;
+  this.j2y = event.offsetY?(event.offsetY):event.pageY-document.getElementById("joystick2").offsetTop;
+}
+
 private joystick1OnTouchStop(event): void {
   this.joystick1Pressed = false;
   this.cx1 = 50;
   this.cy1 = 50;
+}
+
+private joystick2OnTouchStop(event): void {
+  this.joystick2Pressed = false;
+  this.cx2 = 50;
+  this.cy2 = 50;
 }
 
 private joystick1Drag(event): void {
@@ -90,22 +116,38 @@ private joystick1Drag(event): void {
       this.cx1 -= 0.5 * (this.j1x - x);
     }
 
-    //if (((this.j1y - y) < 0 /*&& this.cy1 < 50*/) || ((this.j1y - y) > 0 /*&& this.cy1 < 40)*/) {
     if (((this.j1y - y) > 0 && this.cy1 > 22) || ((this.j1y - y) < 0 && this.cy1 < 70)) {
       this.cy1 -= 0.5 * (this.j1y - y);
     }
-
-    console.log(y);
-    console.log(this.j1y - y);
 
     this.j1x = x;
     this.j1y = y;
   }
 }
 
+private joystick2Drag(event): void {
+  event.preventDefault();
+  if (this.joystick2Pressed) {
+
+    let x = event.offsetX?(event.offsetX):event.pageX-document.getElementById("joystick2").offsetLeft;
+    let y = event.offsetY?(event.offsetY):event.pageY-document.getElementById("joystick2").offsetTop;
+
+    if (((this.j2x - x) > 0 && this.cx2 > 24) || ((this.j2x - x) < 0 && this.cx2 < 75)) {
+      this.cx2 -= 0.5 * (this.j2x - x);
+    }
+
+    if (((this.j2y - y) > 0 && this.cy2 > 22) || ((this.j2y - y) < 0 && this.cy2 < 70)) {
+      this.cy2 -= 0.5 * (this.j2y - y);
+    }
+
+    this.j2x = x;
+    this.j2y = y;
+  }
+}
+
 private onKeydown(event): void {
-    console.log("onKeydown");
-    console.log(event.key);
+    //console.log("onKeydown");
+    //console.log(event.key);
     if (event.key == "ArrowUp") {
       this.cy1 = 25;
     }
@@ -118,11 +160,23 @@ private onKeydown(event): void {
     if (event.key == "ArrowLeft") {
       this.cx1 = 25;
     }
+    if (event.key == "w") {
+      this.cy2 = 25;
+    }
+    if (event.key == "s") {
+      this.cy2 = 75;
+    }
+    if (event.key == "d") {
+      this.cx2 = 75;
+    }
+    if (event.key == "a") {
+      this.cx2 = 25;
+    }
 }
 
 private onKeyup(event): void {
-    console.log("onKeyup");
-    console.log(event.key);
+    //console.log("onKeyup");
+    //console.log(event.key);
     if (event.key == "ArrowUp") {
       this.cy1 = 50;
     }
@@ -134,6 +188,18 @@ private onKeyup(event): void {
     }
     if (event.key == "ArrowLeft") {
       this.cx1 = 50;
+    }
+    if (event.key == "w") {
+      this.cy2 = 50;
+    }
+    if (event.key == "s") {
+      this.cy2 = 50;
+    }
+    if (event.key == "d") {
+      this.cx2 = 50;
+    }
+    if (event.key == "a") {
+      this.cx2 = 50;
     }
 }
 
